784bb3694a1f90d087f5cbf19b34b5e6f33e075c
Aleksandra Duczek <oladuczek231@gmail.com>
2019-04-28 19:11:31 +0200

    Change background, font, etc.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+state = {
+  text: " ",
+  error: "not found"
+}
+
+handleDateChange = (e) => {
+ const value = this.refs.number.value;
+ console.log(value); 
+ /*fetch - ządanie asynchroniczne, tworzy obietnice (promise) - 
+ w czasie tworzenia ma status nierozstrzyggnieta
+ majac odpowiedz od serwera ma status rozstrzygnieta - spelniona odrzucona*/
+ fetch(`http://numbersapi.com/${value}/year?json`)
+ /* po otrzymaniu odp z serwera wywoluje sie metoda then */
+ .then(res => {
+   if(res.ok) {
+     return res;
+   }
+   throw Error(res.status);
+ })
+ .then(res => res.json())
+ .then(data => this.setState({
+   text: data.text
+ }))
+ .catch(err => {
+   this.setState({text: "Error"})
+ })
+
+}
+  render () {
+    return (
+      <div className="style">   
+        <i class="fas fa-landmark"></i>
+        <h1> Enter the year to find out what happened </h1>
+        <h1> in the History </h1>
+        <input onChange = {this.handleDateChange} type="text" ref="number"/>
+        <p> {this.state.text} </p>
+        </div>
+    );
+  }
+
 }
 
 export default App;
